0c8409ada071959ba60b35e78ce35853712898e6
Yelena Hindin <yelenar@gmail.com>
2016-11-11 11:36:06 +0200

    Array iterations

--- hw.js
+++ hw.js
@@ -1,2 +1,12 @@
 #!/usr/bin/env node
 console.log("Hello, world!");
+
+var a = [5, 8,, 12];
+
+for (var l in a)
+    console.log(l, ",", a[l]);
+
+
+for (var l = 0; l < a.length; l++)
+    console.log(l, ",", a[l]);
+
